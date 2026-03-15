33ab3252b0150fa496a95f50bc96d2ba589822a7
dkikowe <dkikowe@gmail.com>
2026-03-15 17:16:20 +0500

    Sds" d

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://melautobackend-production.up.railway.app";
+// export const API_URL = "https://melautobackend-production.up.railway.app";
+export const API_URL = "http://localhost:4040";
 //export const API_URL = "https://srv.educationbridge.kz";
